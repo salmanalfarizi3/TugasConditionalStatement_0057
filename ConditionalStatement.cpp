f90c830ba2285b9cbb987ea0c540380402670821
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-03-10 08:00:16 +0700

    Menambahkan Fungsi untuk melihat kondisi berat badan

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -14,3 +14,17 @@ void inputData(float &berat, float &tinggi) {
 float hitungBMI(float berat, float tinggi) {
     return berat / (tinggi * tinggi);
 }
+
+// 3. Fungsi untuk melihat kondisi berat badan
+string cekStatus(float bmi) {
+    if (bmi < 18.5) {
+        return "Berat Badan Kurang";
+    } else if (bmi < 25) {
+        return "Berat Badan Normal";
+    } else if (bmi < 30) {
+        return "Berat Badan Kelebihan";
+    } else {
+        return "Obesitas";
+    }
+}
+
