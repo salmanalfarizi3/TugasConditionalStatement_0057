1aa9d52a8f600046ba1d2e8f42169621e8f08b8f
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-03-10 07:50:40 +0700

    Menambahkan Prosedur Untuk masukan data

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+// 1. Prosedur untuk masukan data (menggunakan pass-by-reference)
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
